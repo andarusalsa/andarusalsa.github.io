8b882a62c4c2d7cf3e5a690f170686c4f4169c81
andarusalsa <salsa34545@gmail.com>
2024-11-26 21:32:39 +0700

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col, Tab, Nav, Button } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import {Tokoneksi} from "./project/Tokoneksi"
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -24,6 +24,20 @@ export const Projects = () => {
       location:"(Studi Independen, Startup Campus)",
       imgUrl: projImg2,
       url:"/logo"
+    },
+  ];
+  const projectsFE = [
+    {
+      title: "Sistem Informasi Stok Barang",
+      description: "HTML, CSS, Bootstrap",
+      imgUrl: projImg3,
+      url:"https://github.com/Ihza-Fajrur/Sistem-Informasi-Management-Stok"
+    },
+    {
+      title: "Perancangan Antarmuka Website",
+      description: "Next.JS",
+      imgUrl: projImg4,
+      url:"https://github.com/andarusalsa/Tugas-Akhir"
     },
   ];
 
@@ -64,7 +78,7 @@ export const Projects = () => {
                     <Tab.Pane eventKey="second">
                     <Row className="d-flex justify-content-center align-items-center">
                         {
-                          projects.map((project, index) => {
+                          projectsFE.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
