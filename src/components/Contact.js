8c9a488f5a7c7d3ee73f4ae65184b06b6910c39f
andarusalsa <salsa34545@gmail.com>
2024-12-03 19:35:54 +0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,12 +22,12 @@ export const Contact = () => {
         <div>
           <h2>Check My Resume</h2>
           <div className="download-buttons">
-            <a href="https://drive.google.com/file/d/17ZhM0I8QAAw1QT6YMKFaDgk1lZEGv95G/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/file/d/1S896kNb7ZXXrkV60jK5ua3M3g5RQNLEb/view?usp=sharing" target="_blank">
               <button className="download-button">
                 Download CV <img src={USFlag} alt="US Flag" style={{ width: "20px", height: "auto"}} />
               </button>
             </a>
-            <a href="https://drive.google.com/file/d/1hBm5PVLN4RW2dJwE02TPwlCaP4AxPEBp/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/file/d/1w_gGblMzFQk3jR8QFqlRyqEcaFUGuFnG/view?usp=sharing" target="_blank">
               <button className="download-button">
                 Download CV <img src={IDNFlag} alt="Indonesia Flag" style={{ width: "20px", height: "auto" }} />
               </button>
